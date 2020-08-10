b89560bbbc8e5d28da0ceea33836735463c28274
Albin-Joseph <60734163+albin-joseph-wiznet@users.noreply.github.com>
2020-08-10 12:34:58 +0530

    NEW

---
+++ DOCU-REACT-App-Albin/src/pages/index.js
@@ -0,0 +1,12 @@
+/** @format */
+
+import React from 'react';
+//import ReactDOM from 'react-dom';
+import App from '../App';
+export default function Home () {
+return(
+	<App/>
+
+	//document.getElementById('root')
+);
+}
